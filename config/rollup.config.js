e236db6e64006a25a1e56cec01489823c73381f5
luoboding <dengpengks@gmail.com>
2016-11-03 15:37:45 +0800

    [dengpeng]: refactor dirctory name

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -2,10 +2,12 @@ import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-js';
 import config from "./common.config";
 import {argv} from 'yargs';
+import path from 'path'
 
 if (argv.env === "iife") {
   config.moduleName = "rollupVue";
   config.format = "iife";
+  config.dest = path.resolve(__dirname, "../dist/bower", "rollupVue.min.js"),
   config.plugins.push(uglify({}, minify))
 }
 
